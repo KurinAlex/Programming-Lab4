3316abe456a5ffbf234a22aeedcd5499468cca69
Kurin, Alex <olexkurin@gmail.com>
2021-10-18 23:32:06 +0300

    lab4

---
+++ Programming-Lab4/Programming-Lab4.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define RAND_MIN_BOUND -100
+#define RAND_MAX_BOUND 100
+
+int main()
+{
+	srand(time(NULL));
+
+	int size;
+	printf("Enter size:\n");
+	scanf_s("%d", &size);
+
+	double* a = calloc(size, sizeof(double));
+	double scale = (double)(RAND_MAX_BOUND - RAND_MIN_BOUND) / RAND_MAX;
+
+	printf("a: ");
+	for (int i = 0; i < size; i++)
+	{
+		a[i] = rand() * scale + RAND_MIN_BOUND;
+		printf("%lf ", a[i]);
+	}
+
+	int n = size / 2 + size % 2;
+
+	printf("\nresult: ");
+	for (int i = 0, j = size - 1; i < n; i++, j--)
+	{
+		printf("%lf ", a[i] + a[j]);
+	}
+
+	free(a);
+
+	getch();
+	return 0;
+}
